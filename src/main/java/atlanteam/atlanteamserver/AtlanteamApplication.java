7070846b3752da4a4108a9afdf8ddffac560acdc
Romain Camisard <romain.camisard@centrale-marseille.fr>
2021-11-26 13:32:46 +0100

    Romain - First Commit

---
+++ src/main/java/atlanteam/atlanteamserver/AtlanteamApplication.java
@@ -0,0 +1,13 @@
+package atlanteam.atlanteamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlanteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlanteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atlanteam/atlanteamserver/AtlanteamApplicationTests.java
@@ -0,0 +1,13 @@
+package atlanteam.atlanteamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlanteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
