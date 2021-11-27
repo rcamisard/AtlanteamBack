0ffb548a14818ef50bd2b5b916afb166fcb2aa92
Romain Camisard <romain.camisard@centrale-marseille.fr>
2021-11-27 09:15:38 +0100

    Romain - Fix WS

--- src/main/java/atlanteam/atlanteamserver/AppConfiguration.java
+++ src/main/java/atlanteam/atlanteamserver/AppConfiguration.java
@@ -2,14 +2,24 @@ package atlanteam.atlanteamserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class AppConfiguration {
+@EnableWebMvc
+public class AppConfiguration implements WebMvcConfigurer {
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        CorsRegistration mapping = registry.addMapping("/**");
+        mapping.allowedOrigins("*");
+    }
 }
